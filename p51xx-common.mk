2c95dabdc18c19888961ecbfcc8d5962606f3b09
Curtis Menard <curtis.menard@gmail.com>
2012-08-09 20:08:01 -0400

    add prebuilt kernel modules

--- p51xx-common.mk
+++ p51xx-common.mk
@@ -34,6 +34,15 @@ PRODUCT_COPY_FILES := \
 	$(LOCAL_PATH)/init.espresso10.rc:root/init.espresso10.rc \
 	$(LOCAL_PATH)/ueventd.espresso10.rc:root/ueventd.espresso10.rc
 
+# Modules
+PRODUCT_COPY_FILES += \
+   $(LOCAL_PATH)/system/lib/modules/ansi_cprng.ko:system/lib/modules/ansi_cprng.ko \
+   $(LOCAL_PATH)/system/lib/modules/cifs.ko:system/lib/modules/cifs.ko \
+   $(LOCAL_PATH)/system/lib/modules/dhd.ko:system/lib/modules/dhd.ko \
+   $(LOCAL_PATH)/system/lib/modules/rpmsg_client_sample.ko:system/lib/modules/rpmsg_client_sample.ko \
+   $(LOCAL_PATH)/system/lib/modules/rpmsg_client_sample.ko:system/lib/modules/rpmsg_server_sample.ko \
+   $(LOCAL_PATH)/system/lib/modules/scsi_wait_scan.ko:system/lib/modules/scsi_wait_scan.ko
+
 # Vold
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/vold.fstab:system/etc/vold.fstab
